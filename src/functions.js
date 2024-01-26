1c39a27bc1de7de60c0b771b77f81875e622e090
amad <amad@khajistan.com>
2024-01-25 23:56:24 -0500

    wallpaper change works yesssss

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,7 @@
+//set saved wallpaper on load
+document.documentElement.id =
+  'b' + sessionStorage.getItem('wallpaper');
+
 function toggleInfo() {
   let panel = document.getElementById('info-panel-container');
   panel.style.visibility === 'hidden'
@@ -11,20 +15,11 @@ function changeIframe(src) {
   document.getElementById('frame').src = src;
 }
 
-function setInitWallpaper() {
-  console.log('cur wallpaper, ');
-  const currentWallpaper = sessionStorage.getItem('wallpaper');
-  console.log('cur wallpaper, ', sessionStorage.getItem('wallpaper'));
-  document.body.id = 'b' + sessionStorage.getItem('wallpaper');
-}
-
 function setWallpaper() {
-  console.log('hey');
   const currentWallpaper = sessionStorage.getItem('wallpaper');
-  console.log('curr wall', currentWallpaper);
   let num = parseInt(currentWallpaper) || 0;
   num += 1;
   if (num > 12) num = 0;
-  document.body.id = 'b' + num.toString();
+  document.documentElement.id = 'b' + num.toString();
   sessionStorage.setItem('wallpaper', num.toString());
 }
